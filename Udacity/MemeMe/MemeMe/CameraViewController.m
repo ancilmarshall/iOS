be15dd03680b71156769e9f5440e8b67b4dd4324
Ancil <ancilm2000@yahoo.com>
2015-09-16 20:18:48 +0200

    Add RW ReactiveCocoa ReactivePlayground Tutorial

--- Udacity/MemeMe/MemeMe/CameraViewController.m
+++ Udacity/MemeMe/MemeMe/CameraViewController.m
@@ -13,13 +13,17 @@
 @interface CameraViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *topInput;
 @property (weak, nonatomic) IBOutlet UITextField *bottomInput;
-
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation CameraViewController
 
 #pragma mark - view life cycle and rotation
+
+static NSUInteger topInputKVOContext;
+static NSUInteger bottomInputKVOContext;
+static NSUInteger imageViewKVOContext;
 
 - (void) viewDidLoad
 {
@@ -32,6 +36,7 @@
     
     self.navigationItem.rightBarButtonItem = cancelButton;
     self.navigationItem.leftBarButtonItem = postButton;
+    self.navigationItem.leftBarButtonItem.enabled = NO;
     
     self.topInput.delegate = self;
     self.bottomInput.delegate = self;
@@ -39,6 +44,22 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillAppear:)name: UIKeyboardWillShowNotification object: nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillDisappear:) name: UIKeyboardWillHideNotification object: nil];
+    
+    [self addObserver:self
+           forKeyPath:@"topInput.text"
+              options:NSKeyValueObservingOptionNew
+              context:&topInputKVOContext];
+
+    [self addObserver:self
+           forKeyPath:@"bottomInput.text"
+              options:NSKeyValueObservingOptionNew
+              context:&bottomInputKVOContext];
+    
+    [self addObserver:self
+           forKeyPath:@"imageView.image"
+              options:NSKeyValueObservingOptionNew
+              context:&imageViewKVOContext];
+    
 
 }
 
@@ -67,6 +88,11 @@
 {
     [self.topInput resignFirstResponder];
     [self.bottomInput resignFirstResponder];
+    
+    self.topInput.text = @"TOP";
+    self.bottomInput.text = @"BOTTOM";
+    self.imageView.image = nil;
+    
 }
 
 -(void)postButtonPressed;
@@ -78,8 +104,16 @@
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
+    //Post only if the Meme is non-nil
     if (self.imageView.image != nil && self.topInput.text != nil && self.bottomInput.text !=nil) {
 
+        if ( [self.topInput.text isEqualToString:@"TOP"] ||
+            [self.bottomInput.text isEqualToString:@"BOTTOM"]) {
+            [self showAlert:kIncorrectMemeLabels];
+        }
+        
+        
+        
         UIActivityViewController* activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[image] applicationActivities:nil];
         
         [self presentViewController:activityVC animated:YES completion:nil];
@@ -140,7 +174,12 @@
 
 -(void) textFieldDidEndEditing:(UITextField *)textField;
 {
-    
+    if ( [textField.text isEqualToString:@""] ||
+         [textField.text isEqualToString:@"TOP"] ||
+        [textField.text isEqualToString:@"BOTTOM"]) {
+        
+        [self showAlert:kIncorrectMemeLabels];
+    }
 }
 
 -(BOOL) textFieldShouldReturn:(UITextField *)textField;
@@ -199,6 +238,83 @@
 - (void) setImage:(UIImage*)image;
 {
     self.imageView.image = image;
+}
+
+
+# pragma mark - KVO Observation
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context;
+{
+    // Can't unconditionally call super here! Would throw when handling an unexpected keyPath
+    //    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    
+    // Instead, check the context for a match against our private static integer
+    if (!((context == &topInputKVOContext) ||
+        (context == &bottomInputKVOContext) ||
+        (context == &imageViewKVOContext)))
+    {
+        return [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+    
+    // From this point on, guaranteed that all KVO calls are ones that this class registered for
+    NSParameterAssert(object == self);
+    
+    if ([keyPath isEqualToString:@"topInput.text"]){
+        //NSLog(@"topInput.text");
+        if (![self.bottomInput.text isEqualToString:@"BOTTOM"] &&
+            ![self.topInput.text isEqualToString:@"TOP"] &&
+            self.imageView.image != nil )
+        {
+            self.navigationItem.leftBarButtonItem.enabled = YES;
+        }
+        
+    } else if ([keyPath isEqualToString:@"bottomInput.text"]){
+        //NSLog(@"bottomInput.text");
+        if (![self.bottomInput.text isEqualToString:@"BOTTOM"] &&
+            ![self.topInput.text isEqualToString:@"TOP"] &&
+            self.imageView.image != nil )
+        {
+            self.navigationItem.leftBarButtonItem.enabled = YES;
+        }
+        
+    } else if ([keyPath isEqualToString:@"imageView.image"]){
+        //NSLog(@"imageView.image");
+        if (![self.bottomInput.text isEqualToString:@"BOTTOM"] &&
+            ![self.topInput.text isEqualToString:@"TOP"] &&
+            self.imageView.image != nil )
+        {
+            self.navigationItem.leftBarButtonItem.enabled = YES;
+        }
+        
+    } else {
+        NSLog(@"KVO Error");
+    }
+}
+
+#pragma mark - Alert Controller
+static NSString* const kIncorrectMemeLabels = @"IncorrectLabels";
+
+- (void) showAlert:(NSString*)errMsg
+{
+    //instantiate an empty alertController here. Fill in data depending on errMsg
+    UIAlertController* alertController = [UIAlertController
+                                          alertControllerWithTitle:@"" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    
+    if ([errMsg isEqualToString:kIncorrectMemeLabels])
+    {
+        alertController.title = @"Incorrect Meme Headlines";;
+        alertController.message =  @"Please change Meme labels";
+        
+        UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction *action) {
+                                                         }];
+        
+        [alertController addAction:okAction];
+        
+    }
+    
+    [alertController setModalPresentationStyle:UIModalPresentationNone];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
